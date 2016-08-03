bfdc0f70db27cb67afd1e3749b5d683a6d6e6962
razkk <raz.kadr@gmail.com>
2016-08-04 02:06:01 +0300

    added text before printing

--- getline.py
+++ getline.py
@@ -10,8 +10,8 @@ origin_id=cur.fetchone()[0]  #change to fetchone instide all
 find_dest_stop_id="SELECT * FROM stops WHERE stop_code='21127'"
 cur.execute(find_dest_stop_id)
 dest_id=cur.fetchone()[0]
-print origin_id
-print dest_id
+print "Origin id: ",origin_id
+print "Destination id: ",dest_id
 
 find_stop_id_routes="SELECT * FROM stop_times WHERE stop_id=?"
 #origin routes
@@ -19,19 +19,19 @@ cur.execute(find_stop_id_routes, (origin_id,))
 origin_trips=[]
 for row in cur.fetchall():
      origin_trips.append(row[0])
-print origin_trips
+print "Trips that goes through origin station: ",origin_trips
 #destination routs
 cur.execute(find_stop_id_routes, (dest_id,))
 dest_trips=[]
 for row in cur.fetchall():
      dest_trips.append(row[0])
-print dest_trips
+print "Trips that goes through destination station: ",dest_trips
 
 
 shared_trips= [val for val in origin_trips if val in dest_trips]
 shared_trips=set(shared_trips)
 shared_trips=list(shared_trips)
-print shared_trips
+print "Shared trips for both origin and destination: ",shared_trips
 
 find_shared_routes="SELECT * FROM trips WHERE trip_id=?"
 #print shared_trips[0]
@@ -44,9 +44,11 @@ for trip in shared_trips:
 print shared_routes
 
 find_line_from_route="SELECT * FROM routes WHERE route_id=?"
-print "Lines are:"
+lines=[]
 for route in shared_routes:
     cur.execute(find_line_from_route,(route,))
-    res=cur.fetchone()
-    print res[2]
+    res= cur.fetchone()
+    lines.append(res[2])
+
+print "Lines are:", set(lines)
 conn.close()
